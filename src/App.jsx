0e82ee08490d9d65e6a3ee4e2246051d2ba742e8
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-09-08 17:24:24 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 
 import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import userData from './userData.json';
 import friends from './friends.json';
+import transactions from './transactions.json';
 
 
 const App = () => {
@@ -16,6 +18,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
