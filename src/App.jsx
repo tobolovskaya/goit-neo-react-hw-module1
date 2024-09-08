4495b6317115d32a9b9ce7dad5a68c992137915f
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-09-08 17:17:46 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 
 import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
 import userData from './userData.json';
+import friends from './friends.json';
+
 
 const App = () => {
   return (
@@ -12,6 +15,7 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
     </>
   );
 };
---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,15 @@
+import styles from './FriendListItem.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={styles.item}>
+      <img className={styles.avatar} src={avatar} alt={`${name} avatar`} width="80" />
+      <p className={styles.name}>{name}</p>
+      <p className={isOnline ? styles.online : styles.offline}>
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
+    </li>
+  );
+};
+
+export default FriendListItem;
